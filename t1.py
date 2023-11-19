c03b86e91ff100a1c60a26da7368d9a9afd0d473
Roy Chen <roy.chen@duke.edu>
2023-11-18 19:44:58 -0500

    final t1???

--- t1.py
+++ t1.py
@@ -46,6 +46,7 @@ def find_nearest_node(lat1, lon1, node_data):
     return nearest_node
 
 def calculate_route_time(start_node, end_node, graph, current_time):
+    # Returns time it takes to travel from start to end in hours
     distances = {node: float('infinity') for node in graph}
     distances[start_node] = 0
     pq = [(0, start_node)]
@@ -70,8 +71,8 @@ def calculate_route_time(start_node, end_node, graph, current_time):
 
     return float('infinity')
 
-def reinsert_driver(drivers, driver, available):
-    prob = 0.01
+def reinsert_driver(drivers, driver, available, new_loc):
+    prob = 0.80
     rand = random.random()
     if rand > prob: return drivers
 
@@ -87,6 +88,7 @@ def reinsert_driver(drivers, driver, available):
             right = mid - 1
 
     driver['Date/Time'] = available
+    driver['Node'] = new_loc
     drivers.insert(left, driver)
 
     return drivers
@@ -102,20 +104,26 @@ def match_and_calculate_metrics(drivers, passengers, graph, node_data):
         driver = drivers.pop(0)
         passenger = passengers.pop(0)
 
-        driver_node = find_nearest_node(float(driver['Source Lat']), float(driver['Source Lon']), node_data)
-        passenger_pickup_node = find_nearest_node(float(passenger['Source Lat']), float(passenger['Source Lon']), node_data)
-        passenger_dropoff_node = find_nearest_node(float(passenger['Dest Lat']), float(passenger['Dest Lon']), node_data)
+        driver_node = driver['Node']
+        passenger_pickup_node = passenger['Source Node']
+        passenger_dropoff_node = passenger['Dest Node']
 
-        current_time = driver['Date/Time']
-        
-        time_to_passenger = calculate_route_time(driver_node, passenger_pickup_node, graph, current_time)
-        time_to_destination = calculate_route_time(passenger_pickup_node, passenger_dropoff_node, graph, current_time)
+        driver_time = driver['Date/Time']
+        passenger_time = passenger['Date/Time']
 
-        wait_time = time_to_passenger
-        profit_time = time_to_destination - time_to_passenger
+        match_time = max(driver_time, passenger_time)   # datetime object
 
-        available_time = current_time + timedelta(hours=(time_to_passenger + time_to_destination))
-        drivers = reinsert_driver(drivers, driver, available_time)
+        match_wait_time = match_time - passenger_time   # datetime object
+        match_wait_hours = match_wait_time.total_seconds()/3600.0 # wait time in hours
+
+        time_to_passenger = calculate_route_time(driver_node, passenger_pickup_node, graph, match_time) # in hours
+        time_to_destination = calculate_route_time(passenger_pickup_node, passenger_dropoff_node, graph, match_time) # in hours
+
+        wait_time = match_wait_hours + time_to_passenger # in hours
+        profit_time = time_to_destination - time_to_passenger # in hours
+
+        available_time = match_time + timedelta(hours=(time_to_passenger + time_to_destination)) # datetime object
+        drivers = reinsert_driver(drivers, driver, available_time, passenger_dropoff_node)
 
         wait_times.append(wait_time)
         profit_times.append(profit_time)
@@ -125,17 +133,34 @@ def match_and_calculate_metrics(drivers, passengers, graph, node_data):
 
     return average_wait_time, average_profit_time
 
+#%%
+start = time.time()
 adjacency_list = load_json("adjacency.json")
 node_data = load_json("node_data.json")
 drivers_data = load_csv("drivers.csv")
 passengers_data = load_csv("passengers.csv")
+end = time.time()
+print(f'Data Load Time: {end - start: .3} seconds')
+#%%
+start = time.time()
+for d in drivers_data:
+    dlat, dlon = float(d['Source Lat']), float(d['Source Lon'])
+    d['Node'] = find_nearest_node(dlat, dlon, node_data)
 
+for p in passengers_data:
+    pslat, pslon = float(p['Source Lat']), float(p['Source Lon'])
+    pdlat, pdlon = float(p['Dest Lat']), float(p['Dest Lon'])
+    p['Source Node'] = find_nearest_node(pslat, pslon, node_data)
+    p['Dest Node'] = find_nearest_node(pdlat, pdlon, node_data)
+end = time.time()
+print(f'Finding Nearest Nodes of all Drivers/Passengers: {end-start: .3f} seconds')
+#%%
 graph = construct_graph(adjacency_list)
 
 start_time = time.time()
 average_wait_time, average_profit_time = match_and_calculate_metrics(drivers_data, passengers_data, graph, node_data)
 end_time = time.time()
 
-print("Average Wait Time for Passengers (D1):", average_wait_time, "minutes")
-print("Average Profit Time for Drivers (D2):", average_profit_time, "minutes")
+print(f"Average Wait Time for Passengers (D1): {average_wait_time} hours")
+print(f"Average Profit Time for Drivers (D2): {average_profit_time} hours")
 print(f"Runtime (excluding loading data): {end_time - start_time}")
